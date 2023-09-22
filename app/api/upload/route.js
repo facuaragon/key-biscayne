598acb5c02a3ff8604957e13cbd13d27e6de37a9
facuaragon <facundo_aragon@hotmail.com>
2023-09-22 10:22:45 -0300

    se manda correo con archivo

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,4 +1,4 @@
-// import path from "path";
+// ! send video without saving it
 import nodemailer from "nodemailer";
 import { Readable } from "stream";
 
@@ -19,9 +19,12 @@ export async function POST(req) {
     if (!fileBuffer) throw new Error("No se pudo transformar el archivo");
   } catch (error) {
     console.log(error);
-    return new Response(JSON.stringify({ message: "Error uploading file" }), {
-      status: 500,
-    });
+    return new Response(
+      JSON.stringify({ message: "Error al subir el archivo" }),
+      {
+        status: 500,
+      }
+    );
   }
 
   try {
@@ -71,7 +74,92 @@ export async function POST(req) {
   return new Response(JSON.stringify({ message: "Uploading file" }));
 }
 
-// funcion que guarda el video en el directorio
+//! Cloudinary
+// import nodemailer from "nodemailer";
+// import { Readable } from "stream";
+// import cloudinary from "cloudinary";
+
+// // Cloudinary Crendetials
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// export async function POST(req) {
+//   let name;
+//   let fileBuffer;
+
+//   try {
+//     const data = await req.formData();
+//     name = data.get("name");
+//     if (!name) throw new Error("No se pudo obtener el nombre del archivo");
+//     const file = data.get("videoFile");
+//     if (!file) throw new Error("No se pudo obtener el archivo");
+
+//     console.log(name, file);
+
+//     const bytes = await file.arrayBuffer();
+//     fileBuffer = Buffer.from(bytes);
+//     if (!fileBuffer) throw new Error("No se pudo transformar el archivo");
+
+//     // Upload the video to Cloudinary
+//     const cloudinaryUpload = await cloudinary.v2.uploader.upload_stream(
+//       { resource_type: "video" },
+//       async (error, result) => {
+//         if (error) {
+//           console.error(error);
+//           throw new Error("Error al subir el video a Cloudinary");
+//         }
+//         const videoUrl = result.secure_url;
+
+//         // Send the email with the Cloudinary URL
+//         const transporter = nodemailer.createTransport({
+//           host: "smtp.gmail.com",
+//           port: 465,
+//           secure: true,
+//           auth: {
+//             user: process.env.SMTP_EMAIL,
+//             pass: process.env.SMTP_PASSWORD,
+//           },
+//         });
+
+//         await transporter.sendMail({
+//           from: process.env.SMTP_EMAIL,
+//           to: "facuaragon34@gmail.com", // Replace with your recipient's email
+//           subject: `Te han enviado un video`,
+//           html: `
+//           <html>
+//             <body>
+//               <h1>Te han enviado un video</h1>
+//               <p>Revisa el video <a href="${videoUrl}">aquí</a></p>
+//               <video width="320" height="240" controls>
+//         <source src="${videoUrl}" type="video/mp4">
+//         Tu navegador no soporta el elemento de video.
+//       </video>
+//             </body>
+//           </html>
+//         `,
+//         });
+//       }
+//     );
+
+//     const stream = new Readable();
+//     stream.push(fileBuffer);
+//     stream.push(null);
+
+//     stream.pipe(cloudinaryUpload);
+//   } catch (error) {
+//     console.log(error);
+//     return new Response(JSON.stringify({ message: "Error uploading file" }), {
+//       status: 500,
+//     });
+//   }
+
+//   return new Response(JSON.stringify({ message: "Uploading file" }));
+// }
+
+//! funcion que guarda el video en el directorio
 
 // import { writeFile, unlink } from "fs/promises";
 // import path from "path";
